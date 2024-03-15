834ae108302fdbaa0147cf4f017724a642a0669d
virpal12 <virpalsinhofficial@gmail.com>
2024-03-15 19:06:17 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,28 +12,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: false,
       ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text('Share pref'),
-    ),
-      body: Container(
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+var nameValue = "No Value Saved";
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    getValue();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController Name = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Share pref',
+          style: TextStyle(fontSize: 24),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 28.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: Name,
+                decoration: InputDecoration(
+                    hintText: 'Enter your Name',
+                    label: Text('Name'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    var pref = await SharedPreferences.getInstance();
+                    pref.setString("Name", Name.text.toString());
+                  },
+                  child: Text('Save')),
+              SizedBox(
+                height: 70,
+              ),
+              Text(
+                nameValue,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void getValue() async {
+    var prefs = await SharedPreferences.getInstance();
+   // var GetNames = prefs.getString("Name");
+    nameValue = prefs.getString("Name") ?? "No Value Saved";
+    setState(() {});
   }
 }
